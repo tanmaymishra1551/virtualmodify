3257b655b2730ab6c2e5b3f4007a8299e3eb4f24
tanmaymishra1551 <tanmaymishra1551@gmail.com>
2024-06-16 22:48:34 +0530

    Created components of webpage and add bootstarp module

---
+++ frontend/src/components/dashboard/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './assets/styles/main.css';
+
+const Header = () =>{
+    return (
+        <div className='row'>
+
+        </div>
+    );
+};
+
+export default Header;
